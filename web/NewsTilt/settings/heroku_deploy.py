3218020f89d5577a01de33744648fe700211e01f
shinigami1494 <mshah1@cmu.edu>
2018-08-12 21:47:48 +0300

    added django setup to settings

--- web/NewsTilt/settings/heroku_deploy.py
+++ web/NewsTilt/settings/heroku_deploy.py
@@ -8,6 +8,9 @@ ALLOWED_HOSTS = ['news-tilt-backend.herokuapp.com']
 
 INSTALLED_APPS += ['web.NewsTilt.NewsTiltApp',]
 
+import django
+django.setup()
+
 ROOT_URLCONF = 'web.NewsTilt.NewsTiltApp.urls'
 WSGI_APPLICATION = 'web.NewsTilt.wsgi.application'
 
